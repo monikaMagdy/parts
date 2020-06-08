15a80b6f2f196dc4825e3bfbed74a49eef63d6bd
mostafaashraf99 <61276783+mostafaashraf99@users.noreply.github.com>
2020-06-08 16:26:47 +0200

    Update CompanyController.php

--- app/controller/CompanyController.php
+++ app/controller/CompanyController.php
@@ -1,4 +1,36 @@
 <?php
+require_once(__ROOT__ . "controller/Controller.php");
 
+class CompanyController extends Controller
+{
+	public function Con_addCompany()
+	{
+		$CompanyName =filter_var($_REQUEST['CompanyName'], FILTER_SANITIZE_STRING);
+		$email = filter_var($_REQUEST['email'], FILTER_SANITIZE_STRING) ;
+		$phoneNumber=filter_var($_REQUEST['phoneNumber'], FILTER_SANITIZE_EMAIL );
+		$RegisterSupplierNumber = filter_var($_REQUEST['RegisterSupplierNumber'],FILTER_VALIDATE_INT );
+		$CommercialRecord = filter_var($_REQUEST['CommercialRecord'],FILTER_VALIDATE_INT );
+		$this->model->addcompany($CompanyName, $email, $phoneNumber, $RegisterSupplierNumber, $CommercialRecord);
+	}
 
+	public function Con_editCompany($LocalCompanyID)
+	{
+		$CompanyName = $_REQUEST['CompanyName'];
+		$email=$_REQUEST['email'];
+		$phoneNumber=$_REQUEST['phoneNumber'];
+		$RegisterSupplierNumber = $_REQUEST['RegisterSupplierNumber'];
+		$CommercialRecord=$_REQUEST['CommercialRecord'];
+		if (empty( $_REQUEST['CompanyName'])||empty($_REQUEST['email'])||empty($_REQUEST['phoneNumber'])||empty($_REQUEST['RegisterSupplierNumber'])||empty($_REQUEST['CommercialRecord']))
+		echo "<script>alert('Please Fill The empty space');
+		 </script>";
+		}
+	    else
+		$this->model->getCompany($LocalCompanyID)->Model_editCompany($CompanyName,$email,$phoneNumber,$RegisterSupplierNumber,$CommercialRecord);
+	}
+
+	public function Con_delete($LocalCompanyID)
+	{
+		$this->model->getCompany($LocalCompanyID)->deleteCompany($LocalCompanyID);
+	}
+}
 ?>
