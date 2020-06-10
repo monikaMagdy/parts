185eb7b6800a3a5b062ae93e427ecdd9506f7e9e
mostafaashraf99 <61276783+mostafaashraf99@users.noreply.github.com>
2020-06-10 17:20:12 +0200

    Add files via upload

--- app/controller/CompanyController.php
+++ app/controller/CompanyController.php
@@ -6,62 +6,33 @@ class CompanyController extends Controller
 {
 	public function Con_addCompany()
 	{
-		$CompanyName			=filter_var($_REQUEST['CompanyName'], FILTER_SANITIZE_STRING);
-		
-		$email					=filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
-		
-		$phoneNumber			=filter_var($_REQUEST['phoneNumber'],FILTER_VALIDATE_INT );
-		
-		$RegisterSupplierNumber =filter_var($_REQUEST['RegisterSupplierNumber'],FILTER_VALIDATE_INT );
-		
-		$CommercialRecord		=filter_var($_REQUEST['CommercialRecord'],FILTER_VALIDATE_INT );
-		if (strlen($phoneNumber) < 11 || strlen($phoneNumber) > 11)
-		{
-			echo"<script>alert('check phone number');</script>";
-		}
-		else
-			$this->model->addcompany($CompanyName, $email, $phoneNumber, $RegisterSupplierNumber, $CommercialRecord);
+		$CompanyName =filter_var($_REQUEST['CompanyName'], FILTER_SANITIZE_STRING);
+		$email = filter_var($_REQUEST['email'], FILTER_SANITIZE_STRING) ;
+		$phoneNumber=filter_var($_REQUEST['phoneNumber'], FILTER_SANITIZE_EMAIL );
+		$RegisterSupplierNumber = filter_var($_REQUEST['RegisterSupplierNumber'],FILTER_VALIDATE_INT );
+		$CommercialRecord = filter_var($_REQUEST['CommercialRecord'],FILTER_VALIDATE_INT );
+		$this->model->addcompany($CompanyName, $email, $phoneNumber, $RegisterSupplierNumber, $CommercialRecord);
 	}
 
 	public function Con_editCompany($LocalCompanyID)
 	{
-		$CompanyName			=$_REQUEST['CompanyName'];
-		if (!preg_match("/^[a-zA-Z ]*$/",$CompanyName)) 
+		$CompanyName = $_REQUEST['CompanyName'];
+		$email=$_REQUEST['email'];
+		$phoneNumber=$_REQUEST['phoneNumber'];
+		$RegisterSupplierNumber = $_REQUEST['RegisterSupplierNumber'];
+		$CommercialRecord=$_REQUEST['CommercialRecord'];
+		if (empty( $_REQUEST['CompanyName'])||empty($_REQUEST['email'])||empty($_REQUEST['phoneNumber'])||empty($_REQUEST['RegisterSupplierNumber'])||empty($_REQUEST['CommercialRecord']))
 		{
- 			echo "<script>alert('Company Name must be Only letters and white space allowed');
-		 		</script>";
+		echo "<script>alert('Please Fill The empty space');
+		 </script>";
 		}
-		$email					=$_REQUEST['email'];
-		/*{
- 			echo "<script>alert('Invalid email format, @ is a must');
-		 		</script>";
-		}*/
-		$phoneNumber			=$_REQUEST['phoneNumber'];
-		/*if(!preg_match('#^[0-9]+$#', $PartNumber))
-		{
-			echo"<script>alert('you have to enter Numeric Value in Phone Number');
-		 		</script>";
-		}*/
-		$RegisterSupplierNumber =$_REQUEST['RegisterSupplierNumber'];
-		/*if(!preg_match('#^[0-9]+$#', $PartNumber))
-		{
-			echo"<script>alert('you have to enter Numeric Value in Register supplier Number');
-		 		</script>";
-		}*/
-		$CommercialRecord		=$_REQUEST['CommercialRecord'];
-		/*if(!preg_match('#^[0-9]+$#', $PartNumber))
-		{
-			echo"<script>alert('you have to enter Numeric Value in Commercial Record');
-		 		</script>";
-		}
-		
-	    else*/
+	    else
 		$this->model->getCompany($LocalCompanyID)->Model_editCompany($CompanyName,$email,$phoneNumber,$RegisterSupplierNumber,$CommercialRecord);
 	}
 
 	public function Con_delete($LocalCompanyID)
 	{
-		$this->model->getCompany($LocalCompanyID)->deleteCompany();
+		$this->model->getCompany($LocalCompanyID)->deleteCompany($LocalCompanyID);
 	}
 }
 ?>
