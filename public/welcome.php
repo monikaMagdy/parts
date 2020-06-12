f51ab5829663ea05a010cf2d87d80f81860d6f90
mostafaashraf99 <61276783+mostafaashraf99@users.noreply.github.com>
2020-06-12 23:32:28 +0200

    Footer

--- public/welcome.php
+++ public/welcome.php
@@ -66,3 +66,109 @@
 		  </div>
 		</div>
 	  </section>	 
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 class="font-weight-bold text-uppercase mb-4">POLICY</h5>
+        <p>Here you have an article to simplify the process of writing a policy. When you reach the end, you will not only know how to write an effective policy to aid your business management, but you will be all set to implement it..</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="../public/about_us.php">ABOUT US</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="../public/team.php">TEAM</a>
+            </p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fas fa-home mr-3"></i>Cairo, Maadi, ST.262</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-envelope mr-3"></i> admin@auto-spareparts.cf</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-phone mr-3"></i> 01120750444</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-print mr-3"></i> 01120750444</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+        <!-- Facebook -->
+        <a type="button" class="btn-floating btn-fb" href="https://www.facebook.com/?ref=tn_tnmn"> 
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <!-- Twitter -->
+        <a type="button" class="btn-floating btn-ins" href="https://www.instagram.com/">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <!-- Google +-->
+        <a type="button" class="btn-floating btn-gplus" href="https://www.google.com.eg/webhp?hl=en&sa=X&ved=0ahUKEwiA5c2_lf3pAhVHC2MBHXSYC-cQPAgH">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+   
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+
+
+</footer>
+<!-- Footer -->
