eb0977c7e49435e58ef3f37ff2326629fc461fdb
monikaMagdy <monica1702399@miuegypt.edu.eg>
2020-06-12 22:43:17 +0200

    company edit validation and css

--- app/controller/CompanyController.php
+++ app/controller/CompanyController.php
@@ -15,17 +15,10 @@ class CompanyController extends Controller
 		$RegisterSupplierNumber =filter_var($_REQUEST['RegisterSupplierNumber'],FILTER_VALIDATE_INT );
 		
 		$CommercialRecord		=filter_var($_REQUEST['CommercialRecord'],FILTER_VALIDATE_INT );
-		if (strlen($phoneNumber) < 11 || strlen($phoneNumber) > 11)
+		if (empty( $_REQUEST['CompanyName'])||empty($_REQUEST['email'])||empty($_REQUEST['phoneNumber'])||empty($_REQUEST['RegisterSupplierNumber'])||empty($_REQUEST['CommercialRecord']))
 		{
-			echo"<script>alert('check $phoneNumber');</script>";
-		}
-		else if(strlen($RegisterSupplierNumber) !=20)
-		{
-			echo"<script>alert('check $RegisterSupplierNumber');</script>";
-		}
-		else if (strlen($CommercialRecord)!=15)
-		{
-			echo"<script>alert('check $CommercialRecord');</script>";
+		echo "<script>alert('Please Fill The empty space');
+		 </script>";
 		}
 		else
 			$this->model->addcompany($CompanyName, $email, $phoneNumber, $RegisterSupplierNumber, $CommercialRecord);
@@ -34,11 +27,15 @@ class CompanyController extends Controller
 
 	public function Con_editCompany($LocalCompanyID)
 	{
-		$CompanyName = $_REQUEST['CompanyName'];
-		$email=$_REQUEST['email'];
-		$phoneNumber=$_REQUEST['phoneNumber'];
-		$RegisterSupplierNumber = $_REQUEST['RegisterSupplierNumber'];
-		$CommercialRecord=$_REQUEST['CommercialRecord'];
+		$CompanyName			=filter_var($_REQUEST['CompanyName'], FILTER_SANITIZE_STRING);
+		
+		$email					=filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+		
+		$phoneNumber			=filter_var($_REQUEST['phoneNumber'],FILTER_VALIDATE_INT );
+		
+		$RegisterSupplierNumber =filter_var($_REQUEST['RegisterSupplierNumber'],FILTER_VALIDATE_INT );
+		
+		$CommercialRecord		=filter_var($_REQUEST['CommercialRecord'],FILTER_VALIDATE_INT );
 		if (empty( $_REQUEST['CompanyName'])||empty($_REQUEST['email'])||empty($_REQUEST['phoneNumber'])||empty($_REQUEST['RegisterSupplierNumber'])||empty($_REQUEST['CommercialRecord']))
 		{
 		echo "<script>alert('Please Fill The empty space');
--- app/view/CompanyView.php
+++ app/view/CompanyView.php
@@ -197,7 +197,48 @@ public function View_addCompany()
   public function View_editCompany($id)
   {
     $str = "";
-    $str.="<table>";
+    $str.='<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+
+ <title>Auto spare parts</title>
+
+ <!-- Bootstrap core CSS -->
+ <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+ <!-- Custom fonts for this template -->
+ <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css">
+
+ <!-- Custom styles for this template -->
+ <link href="css/agency.min.css" rel="stylesheet">
+
+</head>
+ 
+
+<body id="page-top">
+<br>
+<br>
+<section class="page-section" id="contact">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-12 text-center">
+         <h2 class="section-heading text-uppercase"> Companys</h2>
+         <h3 class="section-subheading text-muted"> </h3>
+       </div>
+     </div>
+     <div class="row">
+       <div class="col-lg-12">';
+   $str.="<table class='section-heading text-uppercase' id='items'>";
     $str.="<tr><th>Company Name</th>
             <th>Email</th>
             <th>Phone Number</th>
@@ -215,9 +256,14 @@ public function View_addCompany()
         $str.="<td><input type='text' name='phoneNumber' value='". $Company->getphoneNumber() ."'></td> ";
          $str.="<td><input type='text' name='RegisterSupplierNumber' value='". $Company->getRegisterSupplierNumber() ."'></td> ";
            $str.="<td><input type='text' name='CommercialRecord' value='". $Company->getCommercialRecord() ."'></td> ";
-        $str.="<td><input type='submit' value='Change'/></td>";
+           $str.=" <div class='portfolio-caption'>
+           <div class='btn-group btn-group-lg'>
+           <td><input type='submit' value='Change'/></td>
+           </div>
+           </div>";
         $str.="</form>";
-        $str.="</tr>";
+        $str.="</tr>
+       ";
       }
       else 
        {
@@ -237,17 +283,24 @@ public function View_addCompany()
         $str.="<td>"
               . $Company->getCommercialRecord() .
               "</td> ";
-        $str.="<td>
+        $str.="
+        <td>
               <input type='submit' value='Change'/>
-              </td>";
+              </td>
+              </div>
+        </div>";
         $str.="<td>
               <a href='Addcompany.php?action=edit&id=".$Company->getLocalCompanyID()."'>Edit</a>
-              </td>";
+              </td>
+              </div>
+        </div>";
         $str.="</tr>";
       }
     }
     $str.="</table>";
-    $str.="<a href='Addcompany.php'>Back to Companys </a>";
+    $str.="<a href='Addcompany.php'>Back to Companys </a>
+    </div>
+    </div>";
     return $str;
   }
 } 
