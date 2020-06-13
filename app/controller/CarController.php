9c7c9391abe9354f430771d03bad7b67ccbdfcd6
monikaMagdy <monica1702399@miuegypt.edu.eg>
2020-06-13 03:52:59 +0200

    car Controller removed validation

--- app/controller/CarController.php
+++ app/controller/CarController.php
@@ -8,21 +8,12 @@ class CarController extends Controller
 	{
 
 		$CarName=$_REQUEST['CarName'];
-		if (!preg_match("/^[a-zA-Z ]*$/",$CarName)) 
-		{
- 			echo "<script>alert('Only letters and white space allowed');
-		 		</script>";
-		}
 
 		$CarModel=$_REQUEST['CarModel'];
 
 		$imgName=$_REQUEST['imgName'];
 		$CarYear=$_REQUEST['CarYear'];
-		if(!preg_match('#^[0-9]+$#', $CarYear))
-		{
-			echo"<script>alert('you have to enter Numeric Value in the CarYear');
-		 		</script>";
-		}else
+		
 		$this->model->addcar($CarName, $CarModel,$CarYear,$imgName);
 		
 	}
@@ -30,21 +21,11 @@ class CarController extends Controller
 	public function editCar($CarID)
 	 {
 	 	$CarName = $_REQUEST["CarName"];
-	 	/*if (!preg_match("/^[a-zA-Z ]*$/",$CarName)) 
-		{
- 			echo "<script>alert('Only letters and white space allowed');
-		 		</script>";
-		}*/
-
+	 	
 		$CarModel = $_REQUEST["CarModel"];
 
 		$CarYear = $_REQUEST["CarYear"];
-		/*	if(filter_var($CarYear, FILTER_VALIDATE_INT))
-		{
-			echo"<script>alert('you have to enter Numeric Value in the Car year');
-		 		</script>";
-		}*/
-	    // else
+	
 		$this->model->getCar($CarID)->Model_editCar($CarName,$CarModel,$CarYear);
 	}
 
